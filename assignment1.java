a92f50849f018ffbf314a5980cc8fb5da4105538
Abdul7068 <142105610+Abdul7068@users.noreply.github.com>
2023-09-04 12:21:19 +0530

    Update assignment1.java

--- assignment1.java
+++ assignment1.java
@@ -1,1 +1,17 @@
 
+package ATB4x1;
+
+public class Lab002
+{
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
